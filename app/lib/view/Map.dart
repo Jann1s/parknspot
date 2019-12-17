9e80568ba185bbf0c5b320c00e6f8f2f5ca5d383
JannisLuegering <dev.luegering@posteo.de>
2019-12-17 20:05:54 +0100

    Reinitiated the project
    Completely recreated the project using the up-to-date version of flutter-stable, Android Studio and the related plugins.
    
    Problems with AndroidX are now solved, the Google Map is integrated into the Map.dart file

--- app/lib/view/Map.dart
+++ app/lib/view/Map.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MyMap extends StatefulWidget {
   State<StatefulWidget> createState() {
@@ -8,19 +11,22 @@ class MyMap extends StatefulWidget {
 }
 
 class MyMapState extends State<MyMap> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _emmenPosition = CameraPosition(
+    target: LatLng(52.78586767371929, 6.8975849999999355),
+    zoom: 14,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'SAD Map',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _emmenPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
