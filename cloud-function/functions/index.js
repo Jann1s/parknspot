c00d44719a0e101c8872a4ed074b55998b441ba7
Jann1s <luegering.jannis@googlemail.com>
2019-12-27 13:51:46 +0100

    Firebase init

--- cloud-function/functions/index.js
+++ cloud-function/functions/index.js
@@ -6,11 +6,3 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-
-exports.createUser = functions.https.onRequest((request, response) => {
-    let email;
-
-    if (request.get('content-type') == 'application/json') {
-        email = request.body.email;
-    }
-});
