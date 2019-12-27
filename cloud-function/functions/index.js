eabd2f56a6af9d5ec5a5baa61814c866ce13952b
Jann1s <luegering.jannis@googlemail.com>
2019-12-27 13:53:03 +0100

    test trigger for Firebase Auth

--- cloud-function/functions/index.js
+++ cloud-function/functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// This will create a User document with the onCreate trigger of Firebase Authentication
+
+exports.createUser = functions.auth.user().onCreate((user) => {
+    let mail = user.email;
+    let time = admin.database.ServerValue.TIMESTAMP;
+
+    admin.firestore().collection('user').add(
+        {
+            email: email,
+            timestamp: time
+        });
+});
