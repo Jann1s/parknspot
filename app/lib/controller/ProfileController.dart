2b18813d8e1c6c2ce65c69eaf13f654c6c1058a9
Jann1s <luegering.jannis@googlemail.com>
2019-12-27 10:13:03 +0100

    Fixed merge conflict

--- app/lib/controller/ProfileController.dart
+++ app/lib/controller/ProfileController.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:parknspot/controller/LoginController.dart';
 import 'package:parknspot/main.dart';
 
@@ -29,6 +30,33 @@ class ProfileController {
     }
     else {
       Main.showToast('Please enter your current E-Mail.');
+      return false;
+    }
+  }
+
+  Future<bool> changePassword(String currentPassword, String newPassword, String confirmPassword) async {
+    try {
+      if (newPassword == confirmPassword) {
+        if (!LoginController.checkInput(newPassword, InputType.Password)) {
+          Main.showToast('Password should be at least 8 char long');
+          return false;
+        }
+        else {
+          FirebaseUser user = _loginController.getUser();
+          String email = user.email;
+
+          AuthResult result = await user.reauthenticateWithCredential(EmailAuthProvider.getCredential(email: email, password: currentPassword));
+          await result.user.updatePassword(newPassword);
+          Main.showToast('Password changed successfully');
+
+          return true;
+        }
+      } else {
+        Main.showToast('Passwords do nomatch');
+        return false;
+      }
+    } catch (error) {
+      print(error);
       return false;
     }
   }
