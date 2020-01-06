401d7e33b05651e2ee8565457d8f66ecd7d4165b
Vadim <vadim.derevicev@student.nhlstenden.com>
2020-01-06 15:52:15 +0100

    Create Business user
    creates business user with apikey, name, location by using http request

--- cloud-function/functions/index.js
+++ cloud-function/functions/index.js
@@ -15,10 +15,55 @@ exports.createUser = functions.auth.user().onCreate((user) => {
         });
 });
 
-exports.deleteUser = functions.auth.user().onDelete(user)
+/* exports.deleteUser = functions.auth.user().onDelete(user)
     .document('users/{uid}')
     ((snap) => {
         return admin.auth().deleteUser(snap.id)
             .then(() => console.log('Deleted user with ID:' + snap.id))
             .catch((error) => console.error('There was an error while deleting user:', error));
     });
+ */
+
+
+// Create Business user
+exports.createBusinessUser = functions.https.onRequest((req, res) => {
+    let apiKey = req.query.apikey;
+    let business = req.query.name;
+    let latitude = parseFloat(req.query.lat);
+    let longitude = parseFloat(req.query.lon);
+    let location = {
+        google: new admin.firestore.GeoPoint(latitude,longitude)
+    };
+
+    admin.firestore().collection('business').add(
+        {
+            apiKey: apiKey,
+            business: business,
+            location: location
+        });
+        
+        res => {
+            console.log(res);
+        }
+});
+
+// Update Business user parking availability
+/* exports.updateBusinessEntry = functions.https.onRequest((req, res) => {
+    let apiKey = null;
+    let parkingAvailability = null;
+    let time = Date.now();
+
+    admin.firestore().collection('business').add(
+        {
+            timestamp: time
+        });
+}); */
+
+// Delete Business user
+/* exports.deleteBusinessUser = functions.https.onRequest(req, res)
+    .document('business/{uid}')
+    ((snap) => {
+        return admin.auth().deleteUser(snap.id)
+            .then(() => console.log('Deleted business user with ID:' + snap.id))
+            .catch((error) => console.error('There was an error while deleting user:', error));
+    }); */
