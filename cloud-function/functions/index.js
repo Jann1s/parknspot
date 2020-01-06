6c77b0e7edb572eac694b92c8bf4269baa634639
MartinTonev <martin.e.tonev@gmail.com>
2020-01-06 06:52:55 +0200

    Update index.js
    Created functions:
    -Set availability
    -Set location
    Both I cant get to respond but they both do their job
    Terminata account is already in here as delete user Jannis did it already but it gives an error

--- cloud-function/functions/index.js
+++ cloud-function/functions/index.js
@@ -14,7 +14,7 @@ exports.createUser = functions.auth.user().onCreate((user) => {
             timestamp: time
         });
 });
-
+/*
 exports.deleteUser = functions.auth.user().onDelete(user)
     .document('users/{uid}')
     ((snap) => {
@@ -22,3 +22,49 @@ exports.deleteUser = functions.auth.user().onDelete(user)
             .then(() => console.log('Deleted user with ID:' + snap.id))
             .catch((error) => console.error('There was an error while deleting user:', error));
     });
+*/
+
+//https://us-central1-parknspot-262413.cloudfunctions.net/setLocation?lat=(latitude goes here)&lon=(longitude goes here)&user=(user id goes here)
+exports.setLocation = functions.https.onRequest(async (req,res) =>{
+    
+    let latitude = parseFloat(req.query.lat);
+    let longitude = parseFloat(req.query.lon);
+    
+    let location = {
+        google: new admin.firestore.GeoPoint(latitude,longitude)
+    }
+    let time = Date.now();
+
+    const snapshot = await admin.firestore().collection('/users').doc(req.query.user).update(
+        {
+            location: location,
+            timestamp: time
+        });
+        res => {
+            console.log(res);
+          },
+          err => {
+            console.log("Error occured");
+          }
+          return;
+}); 
+
+//https://us-central1-parknspot-262413.cloudfunctions.net/setAvailability?parking=(parking id goes here)&availability=(availability goes here)
+exports.setAvailability = functions.https.onRequest(async (req,res) =>{
+
+    let availability = req.query.availability;
+    let time = Date.now();
+
+    const snapshot = await admin.firestore().collection('/locations').doc(req.query.parking).update(
+        {
+            availability: availability,
+            timestamp: time
+        });
+        res => {
+            console.log(res);
+          },
+          err => {
+            console.log("Error occured");
+          }
+          return;
+}); 
