5e9dffe200a83aedae8e5b98f3b42649d49b2471
MartinTonev <martin.e.tonev@gmail.com>
2020-01-06 07:07:53 +0200

    Added some comments and questions

--- cloud-function/functions/index.js
+++ cloud-function/functions/index.js
@@ -14,6 +14,12 @@ exports.createUser = functions.auth.user().onCreate((user) => {
             timestamp: time
         });
 });
+
+/**
+ * deleteUser gives 'user not specified' error when deploying 
+ * TO BE FIXED
+ */
+
 /*
 exports.deleteUser = functions.auth.user().onDelete(user)
     .document('users/{uid}')
@@ -24,6 +30,12 @@ exports.deleteUser = functions.auth.user().onDelete(user)
     });
 */
 
+/**
+ * Enter user id, latitude and longitude as parameter using the https request below to alter the last location of the user
+ * Unset location hasn't been set since it wasn't specified 
+ * Updates timestamp when the location is set
+ * Doesn't provide response for some reason and always times out
+ */
 //https://us-central1-parknspot-262413.cloudfunctions.net/setLocation?lat=(latitude goes here)&lon=(longitude goes here)&user=(user id goes here)
 exports.setLocation = functions.https.onRequest(async (req,res) =>{
     
@@ -48,7 +60,14 @@ exports.setLocation = functions.https.onRequest(async (req,res) =>{
           }
           return;
 }); 
-
+/**
+ * Enter parking id and availability
+ * Availbilty is a string since in firebase I didn't find ENUM 
+ * Updates timestamp when availability is changed
+ * Not too sure on the information you want to send with this one but either way it works 
+ * Doesn't provide response for some reason and always times out
+ * I assume this is what it needs to do 
+ */
 //https://us-central1-parknspot-262413.cloudfunctions.net/setAvailability?parking=(parking id goes here)&availability=(availability goes here)
 exports.setAvailability = functions.https.onRequest(async (req,res) =>{
 
@@ -68,3 +87,13 @@ exports.setAvailability = functions.https.onRequest(async (req,res) =>{
           }
           return;
 }); 
+/**
+ * Haven't done yet 
+ * Still iffy on the details are we saving the parking locations in the database 
+ * I assume this just returns an array with the parkings in the specified radius 
+ * But but but then how/why do we set availability if we don't save them
+ * And if we do save them why? The database would be full of parking locations 
+ */
+exports.getParkingLocations = functions.https.onRequest(async (req,res) => {
+    //TO DO 
+});
